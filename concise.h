153247109ffd9b8752f0b0bf1cf34b74b2cf01e5
Michael Hecher <michael.hecher@gmail.com>
2020-12-13 11:17:15 +0100

    Initial commit.

---
+++ concise.h
@@ -0,0 +1,595 @@
+#pragma once
+#include <assert.h>
+#include <inttypes.h>
+
+#include <array>
+#include <exception>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <memory>
+#include <set>
+#include <string>
+#include <tuple>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#define tn typename
+#define mut auto
+#define var const auto
+#define fix const
+#define me (*this)
+#define ef else if
+
+using i8 = int8_t;
+using i16 = int16_t;
+using i32 = int32_t;
+using i64 = int64_t;
+using u8 = uint8_t;
+using u16 = uint16_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
+using f32 = float;
+using f64 = double;
+using f128 = long double;
+using std::array;
+using std::map;
+using std::set;
+using std::tuple;
+using str = std::string;
+using std::vector;
+template <typename Key,
+          typename T,
+          typename Hash = std::hash<Key>,
+          typename KeyEqual = std::equal_to<Key>,
+          typename Allocator = std::allocator<std::pair<const Key, T>>>
+using hashmap = std::unordered_map<Key, T, Hash, KeyEqual, Allocator>;
+template <typename Key,
+          typename Hash = std::hash<Key>,
+          typename KeyEqual = std::equal_to<Key>,
+          typename Allocator = std::allocator<Key>>
+using hashset = std::unordered_set<Key, Hash, KeyEqual, Allocator>;
+
+template <typename T>
+using ptr = std::shared_ptr<T>;
+template <typename T>
+using uptr = std::unique_ptr<T>;
+template <typename T>
+using wptr = std::weak_ptr<T>;
+
+// clang-format off
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wliteral-suffix"
+int8_t      operator "" b (unsigned long long v) { return (int8_t)v; }
+int16_t     operator "" s (unsigned long long v) { return (int16_t)v; }
+int32_t     operator "" i (unsigned long long v) { return (int32_t)v; }
+uint8_t     operator "" ub(unsigned long long v) { return (uint8_t)v; }
+uint16_t    operator "" us(unsigned long long v) { return (uint16_t)v; }
+uint32_t    operator "" ui(unsigned long long v) { return (uint32_t)v; }
+size_t      operator "" z (unsigned long long v) { return (size_t)v; }
+float       operator "" f (unsigned long long v) { return (float)v; }
+std::string operator "" s (const char* s, std::size_t n) { return std::string{s, n}; }
+static_assert(sizeof(1b) == 1);
+static_assert(sizeof(1s) == 2);
+static_assert(sizeof(1i) == 4);
+static_assert(sizeof(1l) == 8);
+static_assert(sizeof(1ub) == 1);
+static_assert(sizeof(1us) == 2);
+static_assert(sizeof(1ui) == 4);
+static_assert(sizeof(1ul) == 8);
+static_assert(sizeof(1.0) == 8);
+static_assert(sizeof(1.0l) == 16);
+#pragma GCC diagnostic pop
+// clang-format on
+
+template <typename Class, typename T>
+class getter final {
+    typedef const T (*func)(Class&, const T&);
+    typedef void (*del)(Class&, T&);
+
+public:
+    explicit getter(
+        Class& owner,
+        const T value,
+        const func getter = [](Class&, const T& value) -> const T { return value; },
+        const del deleter = [](Class&, T& value) {})
+        : v{value}, c{owner}, g{getter}, d{deleter}
+    {}
+    explicit getter(
+        Class& owner,
+        const T* value,
+        const func getter = [](Class&, const T& value) -> const T { return value; },
+        const del deleter = [](Class&, T& value) {})
+        : v{*value}, c{owner}, g{getter}, d{deleter}
+    {}
+    ~getter()
+    {
+        d(c, v);
+    }
+    operator const T() const
+    {
+        return g(c, v);
+    }
+
+private:
+    T v;
+    Class& c;
+    func g;
+    del d;
+};
+
+template <typename Class, typename T>
+class getset final {
+    typedef const T (*func)(Class&, const T&);
+    typedef void (*del)(Class&, T&);
+
+public:
+    explicit getset(
+        Class& owner,
+        const T value,
+        const func setter = [](Class&, const T& value) -> const T { return value; },
+        const func getter = [](Class&, const T& value) -> const T { return value; },
+        const del deleter = [](Class&, T& value) {})
+        : v{value}, c{owner}, g{getter}, s{setter}, d{deleter}
+    {}
+    explicit getset(
+        Class& owner,
+        const T* value,
+        const func setter = [](Class&, const T& value) -> const T { return value; },
+        const func getter = [](Class&, const T& value) -> const T { return value; },
+        const del deleter = [](Class&, T& value) {})
+        : v{*value}, c{owner}, g{getter}, s{setter}, d{deleter}
+    {}
+    ~getset()
+    {
+        d(c, v);
+    }
+    auto operator=(const T& value) -> const T&
+    {
+        return v = s(c, value);
+    }
+    operator const T() const
+    {
+        return g(c, v);
+    }
+
+private:
+    T v;
+    Class& c;
+    func g, s;
+    del d;
+};
+
+template <typename... T>
+class zip_iterator {
+public:
+    // member typedefs provided through inheriting from std::iterator
+    class iterator
+        : public std::iterator<std::input_iterator_tag,                     // iterator_category
+                               std::tuple<typename T::iterator...>,         // value_type
+                               std::tuple<typename T::iterator...>,         // difference_type
+                               const std::tuple<typename T::iterator...>*,  // pointer
+                               std::tuple<typename T::reference...>         // reference
+                               > {
+        std::tuple<typename T::iterator...> iterators;
+
+    public:
+        explicit iterator(typename T::iterator... iterators)
+            : iterators(std::make_tuple(iterators...))
+        {}
+
+        iterator& operator++()
+        {
+            std::apply([](auto&... iterator) { std::make_tuple((++iterator)...); }, iterators);
+            return *this;
+        }
+
+        iterator operator++(int)
+        {
+            return iterator(std::forward<T>(std::next(iterators))...);
+        }
+
+        bool operator==(const iterator& other) const
+        {
+            return iterators == other.iterators;
+        }
+
+        bool operator!=(const iterator& other) const
+        {
+            return iterators != other.iterators;
+        }
+
+        std::tuple<typename T::reference...> operator*()
+        {
+            return std::apply([](auto&... iterator) { return std::forward_as_tuple(*iterator...); },
+                              iterators);
+        }
+    };
+
+    zip_iterator(T&... containter) : containters(std::forward_as_tuple(containter...))
+    {
+        static_assert(sizeof...(T) > 0, "At least one container is required for zipping.");
+        const auto all_equal = [](auto const& c0, auto const&... c) {
+            return ((c0.size() == c.size()) && ...);
+        }(containter...);
+        if (not all_equal)
+            throw std::invalid_argument("All containers need to have the same size for zipping.");
+    }
+
+    iterator begin()
+    {
+        return std::apply([](auto&... container) { return iterator(container.begin()...); },
+                          containters);
+    }
+
+    iterator end()
+    {
+        return std::apply([](auto&... container) { return iterator(container.end()...); },
+                          containters);
+    }
+
+private:
+    std::tuple<T&...> containters;
+};
+
+template <typename... T>
+class const_zip_iterator {
+public:
+    // member typedefs provided through inheriting from std::iterator
+    class iterator
+        : public std::iterator<std::input_iterator_tag,                    // iterator_category
+                               std::tuple<typename T::const_iterator...>,  // value_type
+                               std::tuple<typename T::const_iterator...>,  // difference_type
+                               const std::tuple<typename T::const_iterator...>*,  // pointer
+                               std::tuple<typename T::const_reference...>         // reference
+                               > {
+        std::tuple<typename T::const_iterator...> iterators;
+
+    public:
+        explicit iterator(typename T::const_iterator... iterators)
+            : iterators(std::make_tuple(iterators...))
+        {}
+
+        iterator& operator++()
+        {
+            std::apply([](auto&... iterator) { std::make_tuple((++iterator)...); }, iterators);
+            return *this;
+        }
+
+        iterator operator++(int)
+        {
+            return iterator(std::forward<T>(std::next(iterators))...);
+        }
+
+        bool operator==(const iterator& other) const
+        {
+            return iterators == other.iterators;
+        }
+
+        bool operator!=(const iterator& other) const
+        {
+            return iterators != other.iterators;
+        }
+
+        std::tuple<typename T::const_reference...> operator*()
+        {
+            return std::apply([](auto&... iterator) { return std::forward_as_tuple(*iterator...); },
+                              iterators);
+        }
+    };
+
+    const_zip_iterator(const T&... containter) : containters(std::forward_as_tuple(containter...))
+    {
+        static_assert(sizeof...(T) > 0, "At least one container is required for zipping.");
+        const auto all_equal = [](auto const& c0, auto const&... c) {
+            return ((c0.size() == c.size()) && ...);
+        }(containter...);
+        if (not all_equal)
+            throw std::invalid_argument("All containers need to have the same size for zipping.");
+    }
+
+    iterator begin()
+    {
+        return std::apply([](auto&... container) { return iterator(container.cbegin()...); },
+                          containters);
+    }
+
+    iterator end()
+    {
+        return std::apply([](auto&... container) { return iterator(container.cend()...); },
+                          containters);
+    }
+
+private:
+    std::tuple<const T&...> containters;
+};
+
+template <typename... T>
+auto zip(T&... containter) -> zip_iterator<T...>
+{
+    return zip_iterator<T...>(containter...);
+}
+
+template <typename... T>
+auto zip(const T&... containter) -> const_zip_iterator<T...>
+{
+    return const_zip_iterator<T...>(containter...);
+}
+
+template <typename... T>
+class enumerated_zip_iterator {
+public:
+    // member typedefs provided through inheriting from std::iterator
+    class iterator
+        : public std::iterator<std::input_iterator_tag,                      // iterator_category
+                               std::tuple<size_t, typename T::iterator...>,  // value_type
+                               std::tuple<size_t, typename T::iterator...>,  // difference_type
+                               const std::tuple<size_t, typename T::iterator...>*,  // pointer
+                               std::tuple<size_t, typename T::reference...>         // reference
+                               > {
+        size_t position;
+        std::tuple<typename T::iterator...> iterators;
+
+    public:
+        explicit iterator(size_t start_position, typename T::iterator... iterators)
+            : position{start_position}, iterators(std::make_tuple(iterators...))
+        {}
+
+        iterator& operator++()
+        {
+            position++;
+            std::apply([](auto&... iterator) { std::make_tuple((++iterator)...); }, iterators);
+            return *this;
+        }
+
+        iterator operator++(int)
+        {
+            return iterator(position + 1, std::forward<T>(std::next(iterators))...);
+        }
+
+        bool operator==(const iterator& other) const
+        {
+            return position == other.position and iterators == other.iterators;
+        }
+
+        bool operator!=(const iterator& other) const
+        {
+            return position != other.position or iterators != other.iterators;
+        }
+
+        std::tuple<size_t, typename T::reference...> operator*()
+        {
+            return std::apply(
+                [this](auto&... iterator) { return std::forward_as_tuple(position, *iterator...); },
+                iterators);
+        }
+    };
+
+    enumerated_zip_iterator(T&... containter) : containters(std::forward_as_tuple(containter...))
+    {
+        static_assert(sizeof...(T) > 0, "At least one container is required for zipping.");
+        const auto all_equal = [](auto const& c0, auto const&... c) {
+            return ((c0.size() == c.size()) && ...);
+        }(containter...);
+        if (not all_equal)
+            throw std::invalid_argument("All containers need to have the same size for zipping.");
+    }
+
+    iterator begin()
+    {
+        return std::apply(
+            [](auto&... container) { return iterator(size_t{0}, container.begin()...); },
+            containters);
+    }
+
+    iterator end()
+    {
+        const auto end_position = std::get<0>(containters).size();
+        return std::apply(
+            [end_position](auto&... container) {
+                return iterator(end_position, container.end()...);
+            },
+            containters);
+    }
+
+private:
+    std::tuple<T&...> containters;
+};
+
+template <typename... T>
+class const_enumerated_zip_iterator {
+public:
+    // member typedefs provided through inheriting from std::iterator
+    class iterator
+        : public std::iterator<
+              std::input_iterator_tag,                                   // iterator_category
+              std::tuple<size_t, typename T::const_iterator...>,         // value_type
+              std::tuple<size_t, typename T::const_iterator...>,         // difference_type
+              const std::tuple<size_t, typename T::const_iterator...>*,  // pointer
+              std::tuple<size_t, typename T::const_reference...>         // reference
+              > {
+        size_t position;
+        std::tuple<typename T::const_iterator...> iterators;
+
+    public:
+        explicit iterator(size_t start_position, typename T::const_iterator... iterators)
+            : position{start_position}, iterators(std::make_tuple(iterators...))
+        {}
+
+        iterator& operator++()
+        {
+            position++;
+            std::apply([](auto&... iterator) { std::make_tuple((++iterator)...); }, iterators);
+            return *this;
+        }
+
+        iterator operator++(int)
+        {
+            return iterator(position + 1, std::forward<T>(std::next(iterators))...);
+        }
+
+        bool operator==(const iterator& other) const
+        {
+            return position == other.position and iterators == other.iterators;
+        }
+
+        bool operator!=(const iterator& other) const
+        {
+            return position != other.position or iterators != other.iterators;
+        }
+
+        std::tuple<size_t, typename T::const_reference...> operator*()
+        {
+            return std::apply(
+                [this](auto&... iterator) { return std::forward_as_tuple(position, *iterator...); },
+                iterators);
+        }
+    };
+
+    const_enumerated_zip_iterator(const T&... containter)
+        : containters(std::forward_as_tuple(containter...))
+    {
+        static_assert(sizeof...(T) > 0, "At least one container is required for zipping.");
+        const auto all_equal = [](auto const& c0, auto const&... c) {
+            return ((c0.size() == c.size()) && ...);
+        }(containter...);
+        if (not all_equal)
+            throw std::invalid_argument("All containers need to have the same size for zipping.");
+    }
+
+    iterator begin()
+    {
+        return std::apply(
+            [](auto&... container) { return iterator(size_t{0}, container.cbegin()...); },
+            containters);
+    }
+
+    iterator end()
+    {
+        const auto end_position = std::get<0>(containters).size();
+        return std::apply(
+            [end_position](auto&... container) {
+                return iterator(end_position, container.cend()...);
+            },
+            containters);
+    }
+
+private:
+    std::tuple<const T&...> containters;
+};
+
+template <typename... T>
+auto ezip(T&... containter) -> enumerated_zip_iterator<T...>
+{
+    return enumerated_zip_iterator<T...>(containter...);
+}
+
+template <typename... T>
+auto ezip(const T&... containter) -> const_enumerated_zip_iterator<T...>
+{
+    return const_enumerated_zip_iterator<T...>(containter...);
+}
+
+template <class TupType, size_t... I>
+void print_tuple(const TupType& _tup, std::index_sequence<I...>)
+{
+    std::cout << "(";
+    (..., (std::cout << (I == 0 ? "" : ", ") << std::get<I>(_tup)));
+    std::cout << ")";
+}
+
+template <class T>
+void print_container(const T& arr)
+{
+    std::cout << '[';
+    for (auto i = arr.begin(); i < arr.end() - 1; ++i)
+        std::cout << *i << ", ";
+    std::cout << *(arr.end() - 1) << ']';
+}
+
+template <class... T>
+std::ostream& operator<<(std::ostream& os, const std::tuple<T...>& tup)
+{
+    print_tuple(tup, std::make_index_sequence<sizeof...(T)>());
+    return os;
+}
+
+template <class T, size_t N>
+std::ostream& operator<<(std::ostream& os, const std::array<T, N>& arr)
+{
+    std::cout << '[';
+    for (auto i = arr.begin(); i != arr.end(); ++i)
+        std::cout << (i == arr.begin() ? "" : ", ") << *i;
+    std::cout << ']';
+    return os;
+}
+
+template <class T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& arr)
+{
+    std::cout << '[';
+    for (auto i = arr.begin(); i != arr.end(); ++i)
+        std::cout << (i == arr.begin() ? "" : ", ") << *i;
+    std::cout << ']';
+    return os;
+}
+
+template <class T>
+std::ostream& operator<<(std::ostream& os, const std::set<T>& arr)
+{
+    std::cout << '{';
+    for (auto i = arr.begin(); i != arr.end(); ++i)
+        std::cout << (i == arr.begin() ? "" : ", ") << *i;
+    std::cout << '}';
+    return os;
+}
+
+template <class K, class V>
+std::ostream& operator<<(std::ostream& os, const std::map<K, V>& arr)
+{
+    std::cout << '{';
+    for (auto i = arr.begin(); i != arr.end(); ++i)
+        std::cout << (i == arr.begin() ? "" : ", ") << i->first << ": " << i->second;
+    std::cout << '}';
+    return os;
+}
+
+template <class T>
+std::ostream& operator<<(std::ostream& os, const std::unordered_set<T>& arr)
+{
+    std::cout << '{';
+    for (auto i = arr.begin(); i != arr.end(); ++i)
+        std::cout << (i == arr.begin() ? "" : ", ") << *i;
+    std::cout << '}';
+    return os;
+}
+
+template <class K, class V>
+std::ostream& operator<<(std::ostream& os, const std::unordered_map<K, V>& arr)
+{
+    std::cout << '{';
+    for (auto i = arr.begin(); i != arr.end(); ++i)
+        std::cout << (i == arr.begin() ? "" : ", ") << i->first << ": " << i->second;
+    std::cout << '}';
+    return os;
+}
+
+template <typename... T>
+auto print()
+{}
+
+template <typename printable_t, typename... T>
+auto print(const printable_t& v, T... values)
+{
+    std::cout << v;
+    print(values...);
+}
+
+template <typename... T>
+auto println(T... values)
+{
+    print(values...);
+    std::cout << '\n';
+}
---
+++ main.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+
+#include "concise.h"
+
+template <tn T>
+auto add(fix T a, fix T b) -> T
+{
+    var c = a + b;
+    return c;
+}
+
+auto addi(fix i32 a, fix i32 b) -> i32
+{
+    return a + b;
+}
+
+class Class {
+public:
+    Class(fix i32 a_, fix i32 b_, fix i32 c_)
+        : a(me, a_, [](Class& self, fix i32& v) -> fix i32 { return v + self.c; })
+        , b(me, b_, [](Class& self, fix i32& v) -> fix i32 { return v - self.c; })
+        , c(c_)
+    {}
+    getter<Class, i32> a;
+    getset<Class, i32> b;
+
+private:
+    i32 c;
+};
+
+auto main() -> int
+{
+    mut c = Class{1, 2, 5};
+    var a = c.a;
+    var b = c.b;
+    c.b = 3;
+    var d = c.b;
+
+    if (d == 2)
+        return 1;
+    ef (d == 3)
+        return 0;
+    else
+        println("Continue");
+
+    var tup = tuple{1i, 1.2l, "test"s};
+    var unique_values = hashset<i32>{1, 2, 3};
+    var basic_map = hashmap<str, i32>{{"k0", 0}, {"k1", 1}, {"k2", 2}};
+    var listi = array{1, 2, 3};
+    var listj = array{1, 2, 3};
+    var listk = vector{1, 2, 3};
+    var string = "This is a string"s;
+
+    var s2 = 123s;
+
+    var i1 = 123i;
+    var i2 = 123l;
+
+    var f1 = 123f;
+    var f2 = 123.0f;
+    var f3 = 123.0;
+    var f4 = 123.0l;
+    var position = 123z;
+
+    println("Tuple: ", tup);
+    println("Set: ", unique_values);
+    println("Map: ", basic_map);
+    println("Array: ", listi);
+    println("Vector: ", listk);
+
+    for (var[i, j, k] : zip(listi, listj, listk)) {
+        println(i, ", ", j, ", ", k);
+    }
+
+    for (var[pos, i, j, k] : ezip(listi, listj, listk)) {
+        println("At position ", pos, ": ", i, ", ", j, ", ", k);
+    }
+
+    return 0;
+}
